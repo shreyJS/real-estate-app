b1ff151e29953f816cb5032c3230cd673fd6c31e
shreyJS <deshpandeshreyas850@gmail.com>
2024-11-11 04:16:44 +0530

    add render config

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
-
+import path from 'path';
 import listingRouter from './routes/listing.route.js';
 
 dotenv.config();
@@ -18,6 +18,7 @@ mongoose
     console.log(err);
   });
 
+  const __dirname = path.resolve()
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -31,6 +32,10 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
+app.use(express.static(path.join(__dirname, '/client/dist')))
+app.get('*',(req,res)=> {
+  res.sendFile(path.join(__dirname, 'client','dist','index.html'))
+})
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
